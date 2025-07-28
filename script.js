4d2ba12b42349ef9c21b06d89ec85dded41d5d0d
brunolorenz <lorenz.bruno@gmail.com>
2025-07-27 21:11:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const url = `${base}&sheet=${sheetName}&tq=${query}`;
 
 // Configurações
 const config = {
-  delayRequisicoes: 5000, // Delay entre requisições à API (1 segundo)
+  delayRequisicoes: 2000, // Delay entre requisições à API (1 segundo)
   zoomPadrao: 12 // Zoom quando seleciona uma cidade
 };
 
